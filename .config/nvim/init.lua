cd741284dfaebbd562b62c64649ec01107bd3026
isteewx <dev.isteewx@gmail.com>
2025-01-18 23:08:39 +0100

    one less plugin

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -261,6 +261,20 @@ require("lazy").setup {
         },
       })
 
+      local hipatterns = require("mini.hipatterns")
+      hipatterns.setup({
+        highlighters = {
+          -- Highlight standalone 'FIXME', 'HACK', 'TODO', 'NOTE'
+          fixme = { pattern = '%f[%w]()FIXME()%f[%W]', group = 'MiniHipatternsFixme' },
+          hack  = { pattern = '%f[%w]()HACK()%f[%W]',  group = 'MiniHipatternsHack'  },
+          todo  = { pattern = '%f[%w]()TODO()%f[%W]',  group = 'MiniHipatternsTodo'  },
+          note  = { pattern = '%f[%w]()NOTE()%f[%W]',  group = 'MiniHipatternsNote'  },
+
+          -- Highlight hex color strings (`#rrggbb`) using that color
+          hex_color = hipatterns.gen_highlighter.hex_color(),
+        },
+      })
+
       local tabline = require("mini.tabline")
       tabline.setup({
         show_icons = false,
@@ -663,13 +677,6 @@ require("lazy").setup {
         },
       }
     end,
-  },
-  -- Highlight todo, notes, etc in comments
-  {
-    "folke/todo-comments.nvim",
-    event = "VimEnter",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    opts = { signs = false },
   },
   { -- Highlight, edit, and navigate code
     "nvim-treesitter/nvim-treesitter",
