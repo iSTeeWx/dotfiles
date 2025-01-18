4411b7b03a87a1a70abd37ec555dfceea5d06ba6
isteewx <dev.isteewx@gmail.com>
2025-01-18 22:47:45 +0100

    prevent mason from being run twice

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -497,7 +497,6 @@ require("lazy").setup {
       --    :Mason
       --
       --  You can press `g?` for help in this menu.
-      require("mason").setup()
 
       -- You can add other tools here that you want Mason to install
       -- for you, so that they are available from within Neovim.
