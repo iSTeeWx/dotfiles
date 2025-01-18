4bdad65fe915ee30ec0a156581a6c16b8638178f
isteewx <dev.isteewx@gmail.com>
2025-01-18 14:32:57 +0100

    replace nvim plugins by their mini counterpart

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -200,6 +200,104 @@ require("lazy").setup {
       },
     },
   },
+  -- { -- Collection of various small independent plugins/modules
+  --  "echasnovski/mini.nvim",
+  --  config = function()
+  --    -- Better Around/Inside textobjects
+  --    --
+  --    -- Examples:
+  --    --  - va)  - [V]isually select [A]round [)]paren
+  --    --  - yinq - [Y]ank [I]nside [N]ext [Q]uote
+  --    --  - ci'  - [C]hange [I]nside [']quote
+  --    require("mini.ai").setup({ n_lines = 500 })
+  --
+  --    -- Add/delete/replace surroundings (brackets, quotes, etc.)
+  --    --
+  --    -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
+  --    -- - sd'   - [S]urround [D]elete [']quotes
+  --    -- - sr)'  - [S]urround [R]eplace [)] [']
+  --    require("mini.surround").setup()
+  --
+  --    -- Simple and easy statusline.
+  --    --  You could remove this setup call if you don't like it,
+  --    --  and try some other statusline plugin
+  --    local statusline = require("mini.statusline")
+  --    -- set use_icons to true if you have a Nerd Font
+  --    statusline.setup({ use_icons = vim.g.have_nerd_font })
+  --
+  --    -- You can configure sections in the statusline by overriding their
+  --    -- default behavior. For example, here we set the section for
+  --    -- cursor location to LINE:COLUMN
+  --    ---@diagnostic disable-next-line: duplicate-set-field
+  --    statusline.section_location = function()
+  --      return "%2l:%-2v"
+  --    end
+  --
+  --    -- ... and there is more!
+  --    --  Check out: https://github.com/echasnovski/mini.nvim
+  --  end,
+  -- },
+  {
+    "echasnovski/mini.nvim",
+    config = function()
+      require("mini.icons").setup()
+      require("mini.pairs").setup()
+
+      require("mini.diff").setup({
+        view = {
+          style = 'sign'
+        },
+        mappings = {
+          -- Disable all mappings
+          apply = '',
+          reset = '',
+          textobject = '',
+          goto_first = '',
+          goto_prev = '',
+          goto_next = '',
+          goto_last = '',
+        },
+      })
+
+      local tabline = require("mini.tabline")
+      tabline.setup({
+        show_icons = false,
+      })
+      vim.cmd("highlight MiniTablineFill guibg=#181825")
+      vim.cmd("highlight MiniTablineCurrent guisp=#181825")
+
+
+      local statusline = require("mini.statusline")
+      ---@diagnostic disable-next-line: duplicate-set-field
+      statusline.section_location = function()
+       return "%l:%v"
+      end
+      statusline.setup({
+        content = {
+          active = function ()
+            local diagnostics_signs = { ERROR = ' ', WARN = ' ', INFO = ' ', HINT = '* ' }
+            local mode, mode_hl = MiniStatusline.section_mode({ trunc_width = 10000 })
+            local git           = MiniStatusline.section_git({ trunc_width = 40 })
+            local diff          = MiniStatusline.section_diff({ trunc_width = 75 })
+            local diagnostics   = MiniStatusline.section_diagnostics({ trunc_width = 75, signs = diagnostics_signs })
+            local filename      = MiniStatusline.section_filename({ trunc_width = 140 })
+            local fileinfo      = MiniStatusline.section_fileinfo({ trunc_width = 120 })
+            local location      = MiniStatusline.section_location({ trunc_width = 75 })
+
+            return MiniStatusline.combine_groups({
+              { hl = mode_hl,                  strings = { mode } },
+              { hl = 'MiniStatuslineDevinfo',  strings = { git, diff, diagnostics } },
+              '%<', -- Mark general truncate point
+              { hl = 'MiniStatuslineFilename', strings = { filename } },
+              '%=', -- End left alignment
+              { hl = 'MiniStatuslineFileinfo', strings = { fileinfo } },
+              { hl = mode_hl,                  strings = { location } },
+            })
+          end
+        }
+      })
+    end,
+  },
   { "sphamba/smear-cursor.nvim", opts = {} },
   {
     "catppuccin/nvim",
@@ -214,37 +312,8 @@ require("lazy").setup {
     opts = { lsp = { progress = { enabled = false } } },
     dependencies = { "MunifTanjim/nui.nvim" },
   },
-  {
-    "nvim-lualine/lualine.nvim",
-    opts = {
-      tabline = {
-        lualine_c = {
-          "buffers",
-        },
-      },
-    },
-  },
-  {
-    "windwp/nvim-autopairs",
-    event = "InsertEnter",
-    config = true,
-  },
   -- "tpope/vim-sleuth", -- Detect tabstop and shiftwidth automatically
-
-  -- See `:help gitsigns` to understand what the configuration keys do
-  { -- Adds git related signs to the gutter, as well as utilities for managing changes
-    "lewis6991/gitsigns.nvim",
-    opts = {
-      signs = {
-        add = { text = "+" },
-        change = { text = "~" },
-        delete = { text = "_" },
-        topdelete = { text = "‾" },
-        changedelete = { text = "~" },
-      },
-    },
-  },
-
+  --
   -- NOTE: Plugins can also be configured to run Lua code when they are loaded.
   --
   -- This is often very useful to both group configuration, as well as handle
@@ -601,44 +670,6 @@ require("lazy").setup {
     dependencies = { "nvim-lua/plenary.nvim" },
     opts = { signs = false },
   },
-
-  -- { -- Collection of various small independent plugins/modules
-  --  "echasnovski/mini.nvim",
-  --  config = function()
-  --    -- Better Around/Inside textobjects
-  --    --
-  --    -- Examples:
-  --    --  - va)  - [V]isually select [A]round [)]paren
-  --    --  - yinq - [Y]ank [I]nside [N]ext [Q]uote
-  --    --  - ci'  - [C]hange [I]nside [']quote
-  --    require("mini.ai").setup({ n_lines = 500 })
-  --
-  --    -- Add/delete/replace surroundings (brackets, quotes, etc.)
-  --    --
-  --    -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
-  --    -- - sd'   - [S]urround [D]elete [']quotes
-  --    -- - sr)'  - [S]urround [R]eplace [)] [']
-  --    require("mini.surround").setup()
-  --
-  --    -- Simple and easy statusline.
-  --    --  You could remove this setup call if you don't like it,
-  --    --  and try some other statusline plugin
-  --    local statusline = require("mini.statusline")
-  --    -- set use_icons to true if you have a Nerd Font
-  --    statusline.setup({ use_icons = vim.g.have_nerd_font })
-  --
-  --    -- You can configure sections in the statusline by overriding their
-  --    -- default behavior. For example, here we set the section for
-  --    -- cursor location to LINE:COLUMN
-  --    ---@diagnostic disable-next-line: duplicate-set-field
-  --    statusline.section_location = function()
-  --      return "%2l:%-2v"
-  --    end
-  --
-  --    -- ... and there is more!
-  --    --  Check out: https://github.com/echasnovski/mini.nvim
-  --  end,
-  -- },
   { -- Highlight, edit, and navigate code
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
