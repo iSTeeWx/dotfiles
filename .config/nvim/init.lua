d46fe05e24a10b7b2861425ce2da2b3df3e04df3
isteewx <dev.isteewx@gmail.com>
2025-01-20 22:38:12 +0100

    compact some things

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -156,50 +156,12 @@ require("lazy").setup {
       notify = {},
     },
     keys = {
-      {
-        "<leader><space>",
-        function()
-          Snacks.picker.recent()
-        end,
-        desc = "Recent Files",
-      },
-      {
-        "<leader>s",
-        function()
-          Snacks.picker.grep()
-        end,
-        desc = "Grep",
-      },
-      {
-        "<leader>q",
-        function()
-          Snacks.picker.diagnostics()
-        end,
-        desc = "Diagnostics",
-      },
-      {
-        "gd",
-        function()
-          Snacks.picker.lsp_definitions()
-        end,
-        desc = "[G]oto [D]efinition",
-      },
-      {
-        "gr",
-        function()
-          Snacks.picker.lsp_references()
-        end,
-        nowait = true,
-        desc = "[G]oto [R]eferences",
-      },
-      {
-        "<F11>",
-        mode = { "n", "t" },
-        function()
-          Snacks.terminal.toggle "zsh"
-        end,
-        desc = "Floating terminal",
-      },
+      { "<leader><space>", function() Snacks.picker.recent() end, desc = "Recent Files" },
+      { "<leader>s", function() Snacks.picker.grep() end, desc = "Grep" },
+      { "<leader>q", function() Snacks.picker.diagnostics() end, desc = "Diagnostics" },
+      { "gd", function() Snacks.picker.lsp_definitions() end, desc = "[G]oto [D]efinition" },
+      { "gr", function() Snacks.picker.lsp_references() end, nowait = true, desc = "[G]oto [R]eferences" },
+      { "<F11>", mode = { "n", "t" }, function() Snacks.terminal.toggle "zsh" end, desc = "Floating terminal" },
     },
   },
   -- { -- Collection of various small independent plugins/modules
