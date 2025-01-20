20b0db7e1f98726587fbb1a075425c2b65b76bd7
isteewx <dev.isteewx@gmail.com>
2025-01-20 22:39:16 +0100

    added incremental selecyion with treesitter

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -629,6 +629,15 @@ require("lazy").setup {
     main = "nvim-treesitter.configs", -- Sets main module to use for opts
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
     opts = {
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = '<CR>',
+          scope_incremental = '<CR>',
+          node_incremental = '<TAB>',
+          node_decremental = '<S-TAB>',
+        }
+      },
       ensure_installed = {
         "bash",
         "c",
